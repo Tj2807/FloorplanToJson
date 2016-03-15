b171553fd26bcfdbcf3447848459d962dc603150
ArpanMajumdar <arpan.gvdn7@gmail.com>
2016-03-16 01:17:37 +0530

    Hough additions

--- source.py
+++ source.py
@@ -22,77 +22,38 @@ def skeleton(img):
         return skel
 
 
-img1 = cv2.imread('FloorPlan1.jpg', 0)
-ret, img2 = cv2.threshold(img1, 70, 255, cv2.THRESH_BINARY_INV)
+img = cv2.imread('FloorPlan2.png')
+img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, img2 = cv2.threshold(img1, 220, 255, cv2.THRESH_BINARY_INV)
 
-element = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+#element = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
 
 # Erosion to remove noise
-img3 = cv2.erode(img2, element, iterations=1)
+#img3 = cv2.erode(img2, element, iterations=1)
 
 # Closing to fill small holes inside foreground
-kernel = np.ones((5, 5), np.uint8)
-img4 = cv2.morphologyEx(img3, cv2.MORPH_CLOSE, kernel)
+#kernel = np.ones((5, 5), np.uint8)
+#img4 = cv2.morphologyEx(img3, cv2.MORPH_CLOSE, kernel)
 
 # Thin the image
-kernel = np.ones((2, 2), np.uint8)
-img5 = cv2.erode(img4, kernel, iterations=2)
+#kernel = np.ones((2, 2), np.uint8)
+#img5 = cv2.erode(img4, kernel, iterations=2)
 
 # Obtain image skeleton
-img6 = skeleton(img5)
+#img6 = skeleton(img5)
 
-## Extract vertices
+#cv2.imshow('img6', img6)
 
-# Find row and column locations that are non-zero
-(rows, cols) = np.nonzero(img6)
+# Hough transform for line detection
+minLineLength = 5
+maxLineGap = 1
+lines = cv2.HoughLinesP(img2, 1, np.pi / 180, 100, minLineLength, maxLineGap)
+print(lines)
+for [[x1, y1, x2, y2]] in lines:
+    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-# Initialize empty list of co-ordinates
-skel_coords = []
 
-# For each non-zero pixel...
-for (r, c) in zip(rows, cols):
-
-    # Extract an 8-connected neighbourhood
-    (col_neigh, row_neigh) = np.meshgrid(np.array([c - 1, c, c + 1]), np.array([r - 1, r, r + 1]))
-
-    # Cast to int to index into image
-    col_neigh = col_neigh.astype('int')
-    row_neigh = row_neigh.astype('int')
-
-    # Convert into a single 1D array and check for non-zero locations
-    pix_neighbourhood = img6[row_neigh, col_neigh].ravel() != 0
-
-    # If the number of non-zero locations equals 2, add this to
-    # our list of co-ordinates
-    if np.sum(pix_neighbourhood) <= 2:
-        skel_coords.append((r, c))
-r, c = img6.shape
-vertices = np.zeros((r, c, 3), 'uint8')
-map = np.zeros((r, c), 'uint8')
-
-for (r, c) in zip(rows, cols):
-    vertices.itemset((r, c, 0), 255)
-    vertices.itemset((r, c, 1), 255)
-    vertices.itemset((r, c, 2), 255)
-
-for (r, c) in skel_coords:
-    vertices.itemset((r, c, 0), 0)
-    vertices.itemset((r, c, 1), 0)
-    vertices.itemset((r, c, 2), 255)
-    map.itemset((r, c), 255)
-
-    # print(k)
-# print('\n\n')
-# (rows, cols) = np.nonzero(vertices)
-# print(rows, cols)
-# print(vertices[:,:,2])
-# map = vertices[:, :, 2]
-map = cv2.dilate(map, kernel, 15)
-vertices[:, :, 2] = map;
-
-cv2.imshow('Figure', vertices)
-cv2.imshow('Vertices', map)
-# cv2.imshow('Dim2', vertices[:, :, 2])
-# cv2.imshow('map', map)
-
+cv2.imshow('img2', img2)
+cv2.imshow('img', img)
 cv2.waitKey(0)
+# cv2.imwrite('houghlines5.jpg',img)
