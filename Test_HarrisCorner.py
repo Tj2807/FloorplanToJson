1d5110743f63ab59178766c9a551dc9da9a83f30
ArpanMajumdar <arpan.gvdn7@gmail.com>
2016-04-03 05:46:12 +0530

    Noise removal using bwareaopen() added

--- Test_HarrisCorner.py
+++ Test_HarrisCorner.py
@@ -21,20 +21,36 @@ img3 = cv2.erode(img2, element, iterations=1)
 # Closing to fill small holes inside foreground
 kernel = np.ones((5, 5), np.uint8)
 img4 = cv2.morphologyEx(img3, cv2.MORPH_CLOSE, kernel)
-#img4 is black and white image
+
+cv2.imshow('img4', img4)
+img4_copy = img4
+_, contours, hierarchy = cv2.findContours(img4, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+for i in contours:
+    area = cv2.contourArea(i)
+    if 0.0 <= area < 100.0:
+        cv2.drawContours(img4_copy, [i], 0, (0, 0, 0), -1)
+    else:
+        cv2.drawContours(img4_copy, [i], 0, (255, 0, 0), -1)
+
+cv2.imshow('img4_modified', img4_copy)
+# img4 is black and white image
 
 # Find Harris corners
 dst = cv2.cornerHarris(np.float32(img4), 5, 5, 0.1)
 r, c = np.nonzero(dst > .5 * dst.max())
+
+"""
 print('r=', r.shape, r)
 print('c=', c.shape, c)
 print('\n\n');
+"""
 
 # Removing nearby corners
 corners = np.array([])
 corners = np.hstack((corners, np.array([r[0], c[0]])))
 corners = np.expand_dims(corners, axis=0)
-print(corners.shape)
+# print(corners.shape)
 for i in range(1, r.shape[0]):
     flag = 0
     for j in range(corners.shape[0]):
@@ -50,8 +66,7 @@ for i in range(1, r.shape[0]):
 
 corners = np.round(corners)
 corners = corners.astype(int)
-print('corners=', corners.shape, corners)
-
+# print('corners=', corners.shape, corners)
 
 # result is dilated for marking the corners, not important
 dst = cv2.dilate(dst, None)
@@ -63,6 +78,6 @@ for k in range(img.shape[2]):
 img5[dst > 0.5 * dst.max()] = [0, 0, 255]
 
 cv2.imshow('img5', img5)
-cv2.imshow('dst', img)
+# cv2.imshow('dst', img)
 if cv2.waitKey(0) & 0xff == 27:
     cv2.destroyAllWindows()
